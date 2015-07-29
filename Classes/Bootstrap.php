45eed98b7cdc4db2adc24e83b47d2f32bf20210a
kevin.schu <kevin.schu@aoe.com>
2015-07-29 15:43:19 +0200

    added custom bootstrap

---
+++ Classes/Bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once dirname(__FILE__) . 'Test/BaseStubTest.php';
+require_once dirname(__FILE__) . 'Test/BaseControllerTest.php';
+require_once dirname(__FILE__) . '../../../../typo3/sysext/core/Build/FunctionalTestsBootstrap.php';
